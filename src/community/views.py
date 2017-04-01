fb8713be5b5c7c11f332ba9c243f412b8ab1949c
Lorence Lim <lorence@ingenuity.ph>
2017-04-01 23:26:13 +0800

    Used raw sql query and showed the groups the user belongs to

--- src/community/views.py
+++ src/community/views.py
@@ -1,5 +1,5 @@
-
 from django.contrib.auth.models import User
+from django.db import connection
 from django.views.generic import TemplateView
 
 
@@ -8,7 +8,56 @@ class CommunityView(TemplateView):
 
     def get_context_data(self, *args, **kwargs):
         context = super(CommunityView, self).get_context_data(*args, **kwargs)
-        context['users'] = User.objects.all()
+        context['users'] = self.get_users()
         return context
+
+    def query_users(self):
+        with connection.cursor() as cursor:
+            cursor.execute(
+                "SELECT\n" +
+                "    `user`.`id`,\n"
+                "    `user`.`username`,\n"
+                "    `user`.`is_staff`,\n"
+                "    `user`.`is_active`,\n"
+                "    `group`.`id`,\n"
+                "    `group`.`name`\n"
+                "FROM `auth_user_groups` AS `user_groups`\n" +
+                "    INNER JOIN `auth_user` AS `user` ON (`user_groups`.`user_id` = `user`.`id`)\n" +
+                "    INNER JOIN `auth_group` AS `group` ON (`user_groups`.`group_id` = `group`.`id`)\n" +
+                "ORDER BY\n" +
+                "    `user`.`id`,\n"
+                "    `group`.`name` DESC;"
+            )
+
+            return cursor.fetchall()
+
+    def get_users(self):
+        result = self.query_users()
+        output = []
+
+        for row in result:
+            record = None
+
+            for item in output:
+                if item['id'] == row[0]:
+                    record = item
+                    break
+
+            if record is None:
+                record = {
+                    'id': row[0],
+                    'username': row[1],
+                    'is_staff': row[2] > 0,
+                    'is_active': row[3] > 0,
+                    'groups': []
+                }
+                output.append(record)
+
+            record['groups'].append({
+                'id': row[4],
+                'name': row[5]
+            })
+
+        return output
 
 community = CommunityView.as_view()
